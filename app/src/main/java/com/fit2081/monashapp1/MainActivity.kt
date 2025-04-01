82a23c6f0757f89e1bc31fe15eb6d857d1139340
homare <honmanihomare@gmail.com>
2025-04-01 15:53:54 +1100

    add login button and author text The first screen should be done

--- app/src/main/java/com/fit2081/monashapp1/MainActivity.kt
+++ app/src/main/java/com/fit2081/monashapp1/MainActivity.kt
@@ -8,7 +8,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -18,21 +17,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.fit2081.monashapp1.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fit2081.monashapp1.ui.theme.Monashapp1Theme
@@ -126,5 +123,23 @@ fun WelcomeScreeen(modifier: Modifier = Modifier) {
             }
         )
 
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                context.startActivity(Intent(context, Loginscreen::class.java))
+            }
+        ) {
+            Text("Login")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Designed by Homare Watanabe (35281375)",
+            fontSize = 16.sp,
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
