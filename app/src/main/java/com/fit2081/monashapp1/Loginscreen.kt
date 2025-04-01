243943f98a460c35139036669de5ee6d33c56e39
homare <honmanihomare@gmail.com>
2025-04-01 13:24:10 +1100

    Add Loginscreen acitivity

--- app/src/main/java/com/fit2081/monashapp1/Loginscreen.kt
+++ app/src/main/java/com/fit2081/monashapp1/Loginscreen.kt
@@ -1,5 +1,3 @@
-// This activity is for welcome screen
-
 package com.fit2081.monashapp1
 
 import android.os.Bundle
@@ -15,14 +13,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.fit2081.monashapp1.ui.theme.Monashapp1Theme
 
-class MainActivity : ComponentActivity() {
+class Loginscreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Monashapp1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    Greeting2(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -33,7 +31,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -42,8 +40,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GreetingPreview2() {
     Monashapp1Theme {
-        Greeting("Android")
+        Greeting2("Android")
     }
 }
